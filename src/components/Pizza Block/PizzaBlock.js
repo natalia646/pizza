e3cdbd0312cd4fd2cd62e9a82fb8f35fe4d5353d
natalia646 <nataxash46@gmail.com>
2023-07-22 11:39:17 +0300

    types

--- src/components/Pizza Block/PizzaBlock.js
+++ src/components/Pizza Block/PizzaBlock.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 
-const PizzaBlock = ({ items }) => {
-  const [pizzaCount, setPizzaCount] = useState(0);
+const PizzaBlock = ({ id, title, image, sizes, types }) => {
+  const pizzaForm = ["тонкі", "стандартні"];
 
   return (
     <div className="all-pizza-block">
-      {items.map((item) => (
-        <article key={item.id} className="pazza-article">
-          <h2>{item.title}</h2>
-          <img src={item.imageUrl} alt={item.title}></img>
-          <button
-            className="button-add"
-            onClick={() => setPizzaCount(pizzaCount + 1)}> + Add {pizzaCount}
-          </button>
-        </article>
-      ))}
+      <article className="pazza-article">
+        <h2>{title}</h2>
+        <img src={image} alt={title}></img>
+        <button className="button-add">Add</button>
+        <div className="pizza-size">
+          <ul>
+            {types.map((type) => (
+              <li key={type}>{pizzaForm[type]}</li>
+            ))}
+          </ul>
+          <ul>
+            {sizes.map((size, i) => (
+              <li key={i}>{size}</li>
+            ))}
+          </ul>
+        </div>
+      </article>
     </div>
   );
 };
