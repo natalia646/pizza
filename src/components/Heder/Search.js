09abf4143e0db9e0062e2fc42d82f0dd01ad58f8
natalia646 <nataxash46@gmail.com>
2023-08-16 16:50:22 +0300

    ref

--- src/components/Heder/Search.js
+++ src/components/Heder/Search.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import style from "../Heder/HederStyles/Search.module.scss";
 import lupa from "../assets/search.svg";
 import close from "../assets/close.svg";
 
 function Search({ valueSearch, changeValueSerch }) {
- 
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    changeValueSerch("");
+    inputRef.current.focus();
+  };
 
   return (
     <div className={style.root}>
@@ -12,9 +17,13 @@ function Search({ valueSearch, changeValueSerch }) {
       <input
         placeholder="Search pizza..."
         value={valueSearch}
-        onChange={(event) => changeValueSerch(event.target.value)}/>
-      {valueSearch && <img src={close} className={style.close} onClick={()=>changeValueSerch('')}></img> }
-      
+        onChange={(event) => changeValueSerch(event.target.value)}
+        ref={inputRef}
+      />
+      {valueSearch && (
+        <img src={close} className={style.close} 
+        onClick={onClickClear}></img>
+      )}
     </div>
   );
 }
