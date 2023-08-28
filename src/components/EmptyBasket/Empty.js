d4d278c67b85c38c6f4c77932fc7d6d099cc5b79
natalia646 <nataxash46@gmail.com>
2023-08-28 12:37:51 +0300

    empty component

--- src/components/EmptyBasket/Empty.js
+++ src/components/EmptyBasket/Empty.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import empty from '../assets/empty.png';
+import style from './Empty.module.scss'
 
 export default function Empty() {
   return (
-    <div>
-      
+    <div className={style.empty}>
+        <h2>Cart Empty</h2>
+      <img src={empty} alt='empty'></img>
     </div>
   )
 }
--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -2,31 +2,45 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import BasketItem from "../components/BasketItem/BasketItem";
-import style from './scss/Basket.module.scss';
+import style from "./scss/Basket.module.scss";
 import { clearItems } from "../redux/slices/basketSlice";
-
-
+import Empty from "../components/EmptyBasket/Empty";
 
 const Basket = () => {
   const pizzaType = ["thin", "standard"];
-  const pizzaSize = [26, 30, 40]
-
+  const pizzaSize = [26, 30, 40];
 
   const dispatch = useDispatch();
   const basketItems = useSelector((state) => state.basket);
-  const {items, totalPrice, totalCount} = basketItems;
+  const { items, totalPrice, totalCount } = basketItems;
 
   const onClikClear = () => {
-    dispatch(clearItems())
+    dispatch(clearItems());
+  };
+
+
+
+  
+  if (!totalCount) {
+    return (
+      <div>
+        <Empty />
+        <Link to="/">Turn around</Link>
+      </div>
+    );
   }
-
-
   return (
     <div>
       <h1>Basket</h1>
       <div className={style.items}>
         {items.map((item) => (
-          <BasketItem key={item.id} {...item} pizzaType = {pizzaType} pizzaSize={pizzaSize}/>))}
+          <BasketItem
+            key={item.id}
+            {...item}
+            pizzaType={pizzaType}
+            pizzaSize={pizzaSize}
+          />
+        ))}
       </div>
 
       <div>
