ef7ff00869a6d21f2fad9b2ae457976f90551eaa
natalia646 <nataxash46@gmail.com>
2023-09-07 10:38:54 +0300

    basket style

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,6 +3,7 @@ import PizzaLogo from "../assets/logo.jpg";
 import { Link } from "react-router-dom";
 import Cart from "../assets/cart.svg";
 import style from "./Logo.module.scss";
+import Line from "../assets/line.svg";
 
 import { useSelector } from "react-redux";
 
@@ -18,12 +19,12 @@ const Logo = () => {
       <Link to="/basket" className={style.basket}>
         <div className={style.totalcount}>
           <img src={Cart}></img>
-          <h2>{totalCount} </h2>
+          <h2 className={style.count}>{totalCount} </h2>
         </div>
-
-        <div className={style.totalprise}>
-          <h2>{totalPrice}</h2>
-          <h3>UAH</h3>
+        <img src={Line} alt="line"></img>
+        <div className={style.totalprice}>
+          <h2 className={style.price}>{totalPrice}</h2>
+          <h3 >UAH</h3>
         </div>
       </Link>
     </header>
