ef7796728a5052630e5ee6b29662798dce9c22eb
natalia646 <nataxash46@gmail.com>
2023-09-12 15:48:05 +0300

    adaptive for Empty

--- src/components/EmptyBasket/Empty.tsx
+++ src/components/EmptyBasket/Empty.tsx
@@ -1,4 +1,3 @@
-
 import empty from "../assets/empty.png";
 import style from "./Empty.module.scss";
 import { Link } from "react-router-dom";
@@ -7,14 +6,14 @@ const Empty: React.FC = () => {
   return (
     <div className={style.empty}>
       <div>
-        <h2>Cart Empty</h2>
+        <h1>Cart Empty</h1>
+        <Link to="/pizza">
+          <p className={style.link}>Turn around</p>
+        </Link>
       </div>
       <img src={empty} alt="empty"></img>
-      <Link to="/pizza" className={style.link}>
-        Turn around
-      </Link>
     </div>
   );
-}
+};
 
-export default Empty
+export default Empty;
